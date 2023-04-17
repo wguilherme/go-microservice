8e7487b53640906fd2744c620c6c9879e9e810b5
wguilherme <wguilhermecriativo@gmail.com>
2023-04-17 15:51:36 -0300

    refactor: add chat config fields

--- internal/domain/entity/chat.go
+++ internal/domain/entity/chat.go
@@ -1,8 +1,14 @@
 package entity
 
-// struct
 type ChatConfig struct {
-	Model *Model
+	Model            *Model
+	Temperature      float32 // 0.0 - 1.0
+	TopP             float32 // 0.0 - 1.0 - to a low value of 0.1 or lower will give more creative results, higher will give more conservative results.
+	N                int     // number of tokens to generate
+	Stop             string  // list of tokens to stop on
+	MaxTokens        int     // number of tokens to generate
+	PresencePenalty  float32 // [optional] -2.0 to 2.0 - Number between -2.0 and 2.0. Positive values penalizea new tokens based on whether they appear in the text so far. Negative values encourage the model to talk about new topics.
+	FrequencyPenalty float32 // [optional] -2.0 to 2.0 - Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far. Negative values encourage the model to talk about less frequent topics.
 }
 
 type Chat struct {
